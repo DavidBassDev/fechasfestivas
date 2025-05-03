8df404c4368e7d16bf28deeca9a14f9d50a47878
unknown <andres.a97@hotmail.com>
2025-05-02 20:15:00 -0500

    compila, agregado tipo servicio en aplicacion, falta controladores

---
+++ aplicacion/src/main/java/servicios/TipoServicio.java
@@ -0,0 +1,59 @@
+package fechasfestivas.api.aplicacion.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fechasfestivas.api.dominio.entidades.*;
+import fechasfestivas.api.core.servicios.*;
+import fechasfestivas.api.infraestructura.repositorios.*;
+
+ 
+
+public class TipoServicio {
+    @Autowired
+    private ITipoRepositorio repositorio;
+
+    public List<Tipo> listar() {
+
+        return repositorio.findAll();
+    };
+
+    public Tipo obtener(int id) {
+
+        return repositorio.findById(id).isEmpty() ? null : repositorio.findById(id).get(); // pregunta si esta vacio, si
+                                                                                           // es asi returna null, si no
+                                                                                           // la funcion findbyId
+    };
+
+    public List<Tipo> buscar(String nombre) {
+
+        return repositorio.buscar(nombre);
+    };
+
+    public Tipo agregar(Tipo nuevoTipo) {
+        nuevoTipo.setId(0);
+        return repositorio.save(nuevoTipo);
+    };
+
+    public Tipo modificar(Tipo tipo) {
+        if (repositorio.findById(tipo.getId()).isEmpty()) {
+            return null;
+        } else {
+            return repositorio.save(tipo);
+        }
+    };
+
+    public boolean eliminar(int id) {
+        try {
+            repositorio.deleteById(id);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    };
+
+    // aca iria el metodo si consultan una fecha, consultar si es festivo o no
+    // validar tambien en el core, ifestivoservicio
+
+}
