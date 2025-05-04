3fdcccdce8ed4e29b255d7a1fd08e98f5258a041
unknown <andres.a97@hotmail.com>
2025-05-04 13:32:13 -0500

    lista la funcion que crea la lista de festivos, segun el año, falta exponer el metodo

--- aplicacion/src/main/java/servicios/FestivoServicio.java
+++ aplicacion/src/main/java/servicios/FestivoServicio.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import fechasfestivas.api.aplicacion.servicios.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import fechasfestivas.api.dominio.entidades.*;
 import fechasfestivas.api.core.servicios.*;
 import fechasfestivas.api.infraestructura.repositorios.*;
+
 
 
 
@@ -56,7 +59,50 @@ public class FestivoServicio implements IFestivoServicio {
         }
     };
 
-    // aca iria el metodo si consultan una fecha, consultar si es festivo o no
-    // validar tambien en el core, ifestivoservicio
+   //se requiere metodo de recibir el año,devuelve una lista construyendo cada festivo de la listaFestivos, segun el tipo de festivo
+   public List<Date> listaFestivos (int año) {
+    Date fecha;
+    ServicioFechas servicio = new ServicioFechas();
+     List<Festivo> listaFestivos = listar();
+     List<Date> nuevaLista = new ArrayList<>();
+
+     for (Festivo festivo : listaFestivos) {
+      int tipoFestivo = festivo.getTipo().getId();
+    switch (tipoFestivo){
+    case 1:
+    //solo agrega el año y a la lista
+    fecha = new Date(año - 1900, festivo.getMes() - 1, festivo.getDia());
+    nuevaLista.add(fecha);
+
+    break;
+
+    case 2:
+    fecha = new Date(año - 1900, festivo.getMes() - 1, festivo.getDia());
+    fecha = servicio.getSiguienteLunes(fecha); 
+    nuevaLista.add(fecha);
+    break;
+
+    case 3:
+
+    fecha = new Date(año - 1900, festivo.getMes() - 1, festivo.getDia());
+
+    fecha= servicio.getInicioSemanaSanta(año-1900);
+
+    nuevaLista.add(fecha);
+    break;
+
+    case 4: 
+    fecha = new Date(año - 1900, festivo.getMes() - 1, festivo.getDia());
+    fecha = servicio.getInicioSemanaSanta(año-1900);
+    fecha = servicio.getSiguienteLunes(fecha);
+    nuevaLista.add(fecha);
+    break;
+    }
+    
+    
+}
+  return nuevaLista;
+
+   }
 
 }
---
+++ aplicacion/src/main/java/servicios/ServicioFechas.java
@@ -0,0 +1,50 @@
+package fechasfestivas.api.aplicacion.servicios;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ServicioFechas {
+
+    // diasQue deben pasar despues del 15 de marzo es decir domingo de ramos: var
+    // dias
+    public static Date getInicioSemanaSanta(int año) {
+
+        int a = año % 19;
+        int b = año % 4;
+        int c = año % 7;
+        int d = (19 * a + 24) % 30;
+
+        int dias = d + (2 * b + 4 * c + 6 * d + 5) % 7;
+
+        int dia = dias + 15;
+        int mes = 3;
+        return new Date(año - 1900, mes - 1, dia);
+
+    }
+
+    public static Date agregarDias(Date fecha,int diasParaSumar) {
+
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(fecha);
+        calendario.add(Calendar.DATE, diasParaSumar);
+        return calendario.getTime();
+
+    }
+
+    public static Date getSiguienteLunes(Date fecha) {
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(fecha);
+        int diaSemana= calendario.get(Calendar.DAY_OF_WEEK);
+        calendario.setTime(fecha);
+        //SI DIA SEMANA COINCIDE NO SE MUEVE
+        if(diaSemana!=Calendar.MONDAY) {
+
+            if(diaSemana>Calendar.MONDAY) {
+                fecha = agregarDias(fecha,9-diaSemana);
+            } else {fecha = agregarDias(fecha,1);}
+            
+        } 
+        return fecha;
+    }
+
+}
